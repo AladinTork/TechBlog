6734be56369f810a7742be707f69fe407ef48428
Arman Tork <canadienperse@gmail.com>
2024-12-16 16:34:39 -0500

    created basic models

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+   { 
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      postId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: Post,
+          key: 'id',
+        },
+        onDelete: 'CASCADE', 
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: User,
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+    }, {
+        sequelize,
+        modelName: 'user',
+        freezeTableName: true,
+        underscored: true,
+        timestamps: false,
+    });
